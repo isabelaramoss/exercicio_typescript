0c13713a32c6a8e2606bc332539c7e8b8571a707
Isabela Ramos <isamrteixeira@gmail.com>
2024-02-17 03:29:57 -0300

    descrição do exercício

--- exercicio.js
+++ exercicio.js
@@ -1,6 +1,8 @@
 "use strict";
+//Função de multiplicação que deverá receber como argumentos dois números e retornar a multiplicação deles.
 const calculaArea = (base, altura) => base * altura;
 calculaArea(10, 15);
+//Função de saudação que deverá receber um nome e retornar a concatenação “Olá" + nome.
 const saudacao = (nome) => {
     console.log(`Olá, ${nome}`);
 };
--- exercicio.ts
+++ exercicio.ts
@@ -1,7 +1,9 @@
+//Função de multiplicação que deverá receber como argumentos dois números e retornar a multiplicação deles.
 const calculaArea = (base:number, altura:number): number => base * altura
 
 calculaArea(10,15);
 
+//Função de saudação que deverá receber um nome e retornar a concatenação “Olá" + nome.
 const saudacao = (nome: string) => {
   console.log(`Olá, ${nome}`);
 }
