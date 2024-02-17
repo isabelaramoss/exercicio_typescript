ae1a9b2a93ddafc73b2ce83645364274a5eede8f
Isabela Ramos <isamrteixeira@gmail.com>
2024-02-17 03:26:46 -0300

    envio de exercício

---
+++ exercicio.js
@@ -0,0 +1,7 @@
+"use strict";
+const calculaArea = (base, altura) => base * altura;
+calculaArea(10, 15);
+const saudacao = (nome) => {
+    console.log(`Olá, ${nome}`);
+};
+saudacao('Isabela');
---
+++ exercicio.ts
@@ -0,0 +1,9 @@
+const calculaArea = (base:number, altura:number): number => base * altura
+
+calculaArea(10,15);
+
+const saudacao = (nome: string) => {
+  console.log(`Olá, ${nome}`);
+}
+
+saudacao('Isabela');
